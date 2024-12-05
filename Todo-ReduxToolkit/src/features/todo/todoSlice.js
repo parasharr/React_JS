197e8decbc02b659da97e6b9be8085e8f614da00
PranjeetGoswami <prgoswami2020@gmail.com>
2024-12-05 10:02:23 +0530

    add to repo

---
+++ Todo-ReduxToolkit/src/features/todo/todoSlice.js
@@ -0,0 +1,29 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos: [{id: 1, text: "Hello World"}]
+}
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        // the state and action are always accessable.
+        // state will provide us the values which are present in our initialState
+        // actions will provide us the ids of retrived values.
+        addTodo: (state, action)=>{
+            const todo = {
+                id: nanoid(),
+                text: action.payload, // payload is an object
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action)=> {
+            state.todos = state.todos.filter((todo)=> todo.id !== action.payload) // match and remove the id after (true) checking and return the remaining values
+        },
+        updateTodo: (state, action)=>{
+            state.todos = state.todos.map((todo)=> 
+                todo.id === action.payload.id ? {...todo, text:action.payload.text} : todo) // For updating the todo we have to use map method
+        }
+    }
+})
