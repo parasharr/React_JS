b522e7e7a3c981a0cb361e296d799f79bae08efb
PranjeetGoswami <prgoswami2020@gmail.com>
2024-12-08 10:00:31 +0530

    add to repo

--- Blogging-app/src/App.jsx
+++ Blogging-app/src/App.jsx
@@ -1,12 +1,37 @@
+import { useState, useEffect } from 'react'
 import './App.css'
+import authService from "./appwrite/auth.js"
+import {useDispatch} from 'react-redux'
+import {login, logout} from "./store/authSlice.js"
+import {Header, Footer} from "./components"
 
 function App() {
-  
-  return (
-    <>
-    <h1>Hello world</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-500'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
