b1feaf93c60efa3edc4e69b2a23508b6b845d9e2
PranjeetGoswami <prgoswami2020@gmail.com>
2025-01-04 21:07:50 +0530

    add to repo

---
+++ react-prac/app/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-pretty text-5xl font-medium uppercase'>
+      page
+    </div>
+  )
+}
+
+export default page
